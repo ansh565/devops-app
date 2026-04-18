e264dacea2ae437680e58b955241c6bb59a600d5
ansh565 <anshbhatialpu@gmail.com>
2026-04-18 17:21:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("Hello from Docker App - CI/CD Test 2");
+  res.end("Hello from Docker App - CI/CD Test 3");
 });
 
 server.listen(3000, () => {
