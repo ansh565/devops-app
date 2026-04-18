8e3975626d044ea64834197224b0d0714e30afca
ansh565 <anshbhatialpu@gmail.com>
2026-04-18 13:22:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Docker App");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
+});
+
+
+
